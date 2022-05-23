91c558a7b162af4e026b44f898cc9a2de0a45192
rkacalski <85071549+rkacalski@users.noreply.github.com>
2022-05-23 16:58:42 -0400

    vehicle parts
    Added vehicle part textures
    Created vehicle part script
    created 1 star vehicle part prefabs

---
+++ Assets/Scripts/vehiclePart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vehiclePart : MonoBehaviour
+{
+    public string partName;
+    public string displayName;
+    public Sprite art;
+    public int rarity;
+    public int cost;
+    public int attack;
+    public int defense;
+    public int accuracy;
+    public int evasion;
+    public int HP;
+    public bool limit;
+}
